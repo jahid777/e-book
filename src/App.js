0fc8d4532ed4af0ba82f96940dc1a9c665e11d5f
unknown <za0162hid@gmail.com>
2022-04-19 14:22:45 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Qr from "./Components/Qr/Qr";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/qr">
+          <Qr />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
