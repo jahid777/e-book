fa5406653cd5675f6a21d619b9acd4634b60b95b
unknown <za0162hid@gmail.com>
2022-04-23 10:27:50 +0600

    clean data

--- src/Components/Qr/Qr.js
+++ src/Components/Qr/Qr.js
@@ -8,33 +8,14 @@ import {
   TextField,
   Button,
 } from "@material-ui/core";
-import QRCode from "qrcode";
 import QrReader from "react-qr-reader";
 
 const Qr = () => {
-  const [text, setText] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
   const [scanResultFile, setScanResultFile] = useState("");
   const [scanResultWebCam, setScanResultWebCam] = useState("");
   const classes = useStyles();
   const qrRef = useRef(null);
 
-  const generateQrCode = async () => {
-    try {
-      const response = await QRCode.toDataURL(text);
-      setImageUrl(response);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  const handleErrorFile = (error) => {
-    console.log(error);
-  };
-  const handleScanFile = (result) => {
-    if (result) {
-      setScanResultFile(result);
-    }
-  };
   const onScanFile = () => {
     qrRef.current.openImageDialog();
   };
@@ -55,48 +36,7 @@ const Qr = () => {
         <CardContent>
           <Grid container spacing={2}>
             <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
-              <TextField
-                label="Enter Text Here"
-                onChange={(e) => setText(e.target.value)}
-              />
-              <Button
-                className={classes.btn}
-                variant="contained"
-                color="primary"
-                onClick={() => generateQrCode()}
-              >
-                Generate
-              </Button>
-              <br />
-              <br />
-              <br />
-              {imageUrl ? (
-                <a href={imageUrl} download>
-                  <img src={imageUrl} alt="img" />
-                </a>
-              ) : null}
-            </Grid>
-            <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
-              <Button
-                className={classes.btn}
-                variant="contained"
-                color="secondary"
-                onClick={onScanFile}
-              >
-                Scan Qr Code
-              </Button>
-              <QrReader
-                ref={qrRef}
-                delay={300}
-                style={{ width: "100%" }}
-                onError={handleErrorFile}
-                onScan={handleScanFile}
-                legacyMode
-              />
-              <h3>Scanned Code: {scanResultFile}</h3>
-            </Grid>
-            <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
-              <h3>Qr Code Scan by Web Cam</h3>
+              <h3>Qr Code Scan by Web Cam to Open Ebook</h3>
               <QrReader
                 delay={300}
                 style={{ width: "100%" }}
