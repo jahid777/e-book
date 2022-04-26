33b4febb91389b2d989dbaf2675d1cb02edfce74
unknown <za0162hid@gmail.com>
2022-04-26 12:01:26 +0600

    authentication full done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,6 +7,8 @@ const Home = () => {
   const [scanResultWebCam, setScanResultWebCam] = useState("");
   const [display, setDisply] = useState(false);
   const [inputData, setInputData] = useState("");
+  const [finalInputData, setFinalInputData] = useState("");
+  const [errorMessage, setErrorMessage] = useState(false);
   const history = useHistory();
 
   const handleErrorWebCam = (error) => {
@@ -19,21 +21,18 @@ const Home = () => {
   };
 
   const data = "FvjfsmsvVsbHsbJsban";
-
-  // const storeAuthToken = () => {
-  //   firebase.auth().currentUser.getIdToken(/* forceRefresh */ true)
-  //     .then(function (idToken) {
-  //       sessionStorage.setItem('token', idToken);
-  //       history.replace(from);
-  //     }).catch(function (error) {
-  //       // Handle error
-  //     });
-  // }
-
-  if (scanResultWebCam || inputData == data) {
+  if (scanResultWebCam || finalInputData == data) {
     sessionStorage.setItem("token", data);
     history.push("/termsCondition");
   }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFinalInputData(inputData);
+    if (finalInputData != data) {
+      setErrorMessage(true);
+    }
+  };
 
   return (
     <div>
@@ -58,79 +57,33 @@ const Home = () => {
         </button>
       </div>
 
-      <input
-        type="text"
-        className="form-control container mt-5"
-        onChange={(e) => setInputData(e.target.value)}
-      />
+      <form onSubmit={handleSubmit}>
+        <div class="input-group container mt-5">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Recipient's username"
+            aria-label="Recipient's username"
+            aria-describedby="button-addon2"
+            onChange={(e) => setInputData(e.target.value)}
+          />
+          <button className="btn btn-primary" type="submit">
+            SUBMIT
+          </button>
+        </div>
+      </form>
+      {errorMessage && <h1>INVALID PASSWORD</h1>}
     </div>
   );
 };
 
 export default Home;
 
-// import React, { useState, useRef } from "react";
-// import {
-//   Container,
-//   Card,
-//   CardContent,
-//   makeStyles,
-//   Grid,
-// } from "@material-ui/core";
-// import QrReader from "react-qr-reader";
-
-// const Qr = () => {
-//   const [scanResultWebCam, setScanResultWebCam] = useState("");
-//   const classes = useStyles();
-//   const qrRef = useRef(null);
-
-//   const handleErrorWebCam = (error) => {
-//     console.log(error);
-//   };
-//   const handleScanWebCam = (result) => {
-//     if (result) {
-//       setScanResultWebCam(result);
-//     }
-//   };
-//   return (
-//     <Container className={classes.conatiner}>
-//       <Card>
-//         <h2 className={classes.title}>Ebook</h2>
-//         <CardContent>
-//           <Grid container spacing={2}>
-//             <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
-//               <h3>Qr Code Scan by Web Cam to Open Ebook</h3>
-//               <QrReader
-//                 delay={300}
-//                 style={{ width: "100%" }}
-//                 onError={handleErrorWebCam}
-//                 onScan={handleScanWebCam}
-//               />
-//               <h3>Scanned By WebCam Code: {scanResultWebCam}</h3>
-//             </Grid>
-//           </Grid>
-//         </CardContent>
-//       </Card>
-//     </Container>
-//   );
-// };
-
-// const useStyles = makeStyles((theme) => ({
-//   conatiner: {
-//     marginTop: 10,
-//   },
-//   title: {
-//     display: "flex",
-//     justifyContent: "center",
-//     alignItems: "center",
-//     background: "#3f51b5",
-//     color: "#fff",
-//     padding: 20,
-//   },
-//   btn: {
-//     marginTop: 10,
-//     marginBottom: 20,
-//   },
-// }));
-
-// export default Qr;
+// <input
+//           type="text"
+//           className="form-control container mt-5"
+//           onChange={(e) => setInputData(e.target.value)}
+//         />
+//         <button className="btn btn-primary" type="submit">
+//           Submit
+//         </button>
