7a1cef494ca9f63761943a7dbfbc368df2652b6c
unknown <za0162hid@gmail.com>
2022-04-27 15:57:23 +0600

    page created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import AddBook from "./Components/AddBook/AddBook";
+import AddBook from "./Components/AddData/AddBook";
+import TermsConditionEdit from "./Components/EditData/TermsConditionEdit";
+
 import Home from "./Components/Home/Home";
 import PrivateRoute from "./Components/PrivateRoute/PrivateRoute";
 import TermsCondition from "./Components/TermsCondition/TermsCondition";
@@ -12,14 +14,16 @@ function App() {
         <Route exact path="/">
           <Home />
         </Route>
-
         <PrivateRoute path="/termsCondition">
           <TermsCondition />
         </PrivateRoute>
-
         <PrivateRoute path="/addBook">
           <AddBook />
         </PrivateRoute>
+        TermsConditionEdit
+        <Route path="/termsConditionEdit">
+          <TermsConditionEdit />
+        </Route>
       </Switch>
     </Router>
   );
