feac653cd26945712505f6374f3242bc7e256a80
unknown <za0162hid@gmail.com>
2022-05-06 13:11:44 +0600

    display book banner edit

--- src/Components/EditData/BannerEditDisplaybook.js
+++ src/Components/EditData/BannerEditDisplaybook.js
@@ -1,8 +1,10 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 
 const BannerEditDisplaybook = () => {
   const topImgRef = useRef();
   const [suceesMsg, setSuccessMsg] = useState(false);
+  const [topBannerImg, setTopBannerImg] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   //top banner submission
   const handleTopBannerSubmit = () => {
@@ -22,6 +24,36 @@ const BannerEditDisplaybook = () => {
         }
       });
   };
+
+  //getting the top banner img
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          "http://localhost:5000/DisplayBookTopImage"
+        );
+        const data = await response.json();
+        setTopBannerImg(data);
+      } catch (error) {
+        console.log("err", error);
+      }
+      setLoading(false);
+    };
+    fetchProduct();
+  }, []);
+
+  const handleTopImageRemove = (id) => {
+    // console.log('deleted',id);
+    fetch(`http://localhost:5000/bookDisplayImgdelete/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log("deleted successfully");
+      });
+    window.location.reload();
+  };
   return (
     <div>
       {/* top banner form */}
@@ -29,25 +61,97 @@ const BannerEditDisplaybook = () => {
       <form onSubmit={handleTopBannerSubmit}>
         <div className="mb-3 mt-5 container">
           <label htmlFor="exampleFormControlTextarea1" className="form-label">
-            <h3>ADD YOUR DISPLAY BOOK IMAGE</h3>
+            <h3>Add your display book banner</h3>
           </label>
           <div className="input-group mb-3">
             <input
               required
               type="text"
               className="form-control"
-              placeholder="pest your image link"
+              placeholder="Your image link"
               aria-label="Recipient's username"
               id="exampleFormControlTextarea1"
               aria-describedby="button-addon2"
               ref={topImgRef}
             />
+
             <button
-              className="btn btn-outline-secondary"
+              className="btn btn-outline-primary"
+              type="button"
+              id="button-addon2"
+              data-bs-toggle="modal"
+              data-bs-target="#staticBackdropTopBanner"
+            >
+              Delete
+            </button>
+            {/* <!-- Modal --> */}
+            <div
+              className="modal fade"
+              id="staticBackdropTopBanner"
+              data-bs-backdrop="static"
+              data-bs-keyboard="false"
+              tabIndex="-1"
+              aria-labelledby="staticBackdropLabel"
+              aria-hidden="true"
+            >
+              <div className="modal-dialog modal-dialog-centered">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title" id="staticBackdropLabel">
+                      Top display book banner
+                    </h5>
+                    <button
+                      type="button"
+                      className="btn-close"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    ></button>
+                  </div>
+                  <div
+                    className="modal-body"
+                    style={{ display: "flex", justifyContent: "center" }}
+                  >
+                    {/* this is modal body */}
+
+                    <div
+                      className="card"
+                      style={{
+                        width: "18rem",
+                      }}
+                    >
+                      {topBannerImg.map((tpData) => (
+                        <span key={tpData?._id}>
+                          <img
+                            src={tpData?.topdisplayBookBanner}
+                            className="card-img-top img-fluid"
+                            style={{
+                              height: "150px",
+                            }}
+                            alt="..."
+                          />
+
+                          <button
+                            type="button"
+                            className="btn btn-primary"
+                            style={{ margin: "10px 20px" }}
+                            onClick={() => handleTopImageRemove(tpData._id)}
+                          >
+                            Confirm Delete
+                          </button>
+                        </span>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <button
+              className="btn btn-outline-primary"
               type="submit"
               id="button-addon2"
             >
-              Button
+              Submit
             </button>
           </div>
         </div>
